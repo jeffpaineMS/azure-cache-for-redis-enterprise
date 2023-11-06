6fcd562ea4ac1f8a8d0cbfff6f8504138f698f97
Jeff Paine <jeff.paine@gmail.com>
2023-11-06 17:37:01 -0500

    spring and redis

---
+++ infra/provider.tf
@@ -0,0 +1,20 @@
+# Azure provider version 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 3.75.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    log_analytics_workspace {
+      permanently_delete_on_destroy = true
+    }
+  }
+}
